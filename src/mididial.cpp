516ff1dac55aabcd93140dded397132809c038f0
MajorX234 <majorx234@googlemail.com>
2022-03-10 21:51:40 +0100

    Add small QDial example for future midi usage

---
+++ src/mididial.cpp
@@ -0,0 +1,40 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//    This file is part of QtJack.                                           //
+//    Copyright (C) 2014-2015 Jacob Dawid <jacob@omg-it.works>               //
+//                  2022 <majorx234@googlemail.com>                          //
+//    QtJack is free software: you can redistribute it and/or modify         //
+//    it under the terms of the GNU General Public License as published by   //
+//    the Free Software Foundation, either version 3 of the License, or      //
+//    (at your option) any later version.                                    //
+//                                                                           //
+//    QtJack is distributed in the hope that it will be useful,              //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of         //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+//    GNU General Public License for more details.                           //
+//                                                                           //
+//    You should have received a copy of the GNU General Public License      //
+//    along with QtJack. If not, see <http://www.gnu.org/licenses/>.         //
+//                                                                           //
+//    It is possible to obtain a closed-source license of QtJack.            //
+//    If you're interested, contact me at: jacob@omg-it.works                //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include <QApplication>
+#include <QWidget>
+#include <QDial>
+
+#include "ui_mididial.h"
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+    QWidget window;
+    Ui::Mididial mididial_ui;
+    mididial_ui.setupUi(&window);
+    QAbstractSlider *slider = mididial_ui.dial;
+    slider->setAttribute(Qt::WA_AcceptTouchEvents);
+    window.show();
+    return app.exec();
+}
