67e60d0caee47e800b6db0a47f7bc91e1cde079f
MajorX234 <majorx234@googlemail.com>
2022-03-14 11:12:41 +0100

    Add midi message sending with dial value

--- src/mididial_mainwindow.cpp
+++ src/mididial_mainwindow.cpp
@@ -33,7 +33,7 @@ MidiDialMainWindow::MidiDialMainWindow(QWidget *parent)
     , Processor(_client) 
     , mididial_ui(new Ui::Mididial)
 {
-	mididial_ui->setupUi(this);
+    mididial_ui->setupUi(this);
     slider = mididial_ui->dial;
     slider->setAttribute(Qt::WA_AcceptTouchEvents);
     setCentralWidget(slider);
@@ -42,22 +42,37 @@ MidiDialMainWindow::MidiDialMainWindow(QWidget *parent)
 }
 
 MidiDialMainWindow::~MidiDialMainWindow(){
-
+    delete _midi_out_buffer;
+    _client.disconnectFromServer();
 }
 
 void MidiDialMainWindow::setupJackClient(){
     _client.connectToServer("mididial");
     _midi_out = _client.registerMidiOutPort("out_1");
+    _midi_out_buffer = new QtJack::MidiBuffer(); //not used yet
     _client.setMainProcessor(this);
     _client.activate();
 }
 
-
 void MidiDialMainWindow::on_twist() {
-  int value = mididial_ui->dial->value();
-  printf("dial value: %d\n",value);
+    _value = mididial_ui->dial->value();
+    _timestamp = _client.getJackFrameTime();
 }
 
 void MidiDialMainWindow::process(int samples) {
 
+    jack_nframes_t last_frame_time;
+    QtJack::MidiBuffer port_buffer = _midi_out.buffer(samples);
+    port_buffer.clearEventBuffer();
+    last_frame_time = _client.getJackTime();     
+    unsigned int printvalue = _value;
+    int timestamp = _timestamp;
+    int t = timestamp + samples - last_frame_time;
+    if((t > 0) && (t < samples) ){
+        unsigned char midiData[3];
+        midiData[0] = 0xb1;
+        midiData[1] = 10;
+        midiData[2] = 0xff & printvalue;
+        port_buffer.writeEvent(t, midiData, 3);
+    }    
 }
