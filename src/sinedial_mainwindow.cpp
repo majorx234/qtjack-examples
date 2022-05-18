fc668cb2e563ab3785b6847409ffb59777b413ae
MajorX234 <majorx234@googlemail.com>
2022-05-18 19:22:48 +0200

    Add sine wave generation in timer function, add ringbuffer functionality

--- src/sinedial_mainwindow.cpp
+++ src/sinedial_mainwindow.cpp
@@ -20,7 +20,9 @@
 //    If you're interested, contact me at: jacob@omg-it.works                //
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
+
 #include <QtWidgets/QMainWindow>
+#include <QDebug>
 #include <Client>
 #include <QAbstractSlider>
 #include "sine_wave.hpp"
@@ -31,25 +33,29 @@ SineDialMainWindow::SineDialMainWindow(QWidget *parent)
     , Processor(_client)
     , sinedial_ui(new Ui::Sinedial)
     , sample_rate(48000)
-    , generated_samples_per_tick(1024)
-    , samples_offset(0)
+    , generated_samples_per_tick(2500)
+    , samples_offset_left(0)
+    , samples_offset_right(0)
 {
-    left = (QtJack::AudioSample*)malloc(generated_samples_per_tick* sizeof(QtJack::AudioSample));
-    right = (QtJack::AudioSample*)malloc(generated_samples_per_tick* sizeof(QtJack::AudioSample));
+    left = (QtJack::AudioSample*)malloc(
+        generated_samples_per_tick* sizeof(QtJack::AudioSample));
+    right = (QtJack::AudioSample*)malloc(
+        generated_samples_per_tick* sizeof(QtJack::AudioSample));
     sinedial_ui->setupUi(this);
     slider_base_freq = sinedial_ui->dial_base_freq;
     slider_mod_freq = sinedial_ui->dial_mod_freq;
     slider_base_freq->setAttribute(Qt::WA_AcceptTouchEvents);
     slider_mod_freq->setAttribute(Qt::WA_AcceptTouchEvents);
+    startTimer(50); //tick all 200ms (faster than needed 250ms)
 
     setupJackClient();
-    connect(slider_base_freq, &QDial::valueChanged, this, &SineDialMainWindow::on_twist);
-    connect(slider_mod_freq, &QDial::valueChanged, this, &SineDialMainWindow::on_twist);
+    connect(slider_base_freq, &QDial::valueChanged,
+            this, &SineDialMainWindow::on_twist);
+    connect(slider_mod_freq, &QDial::valueChanged,
+            this, &SineDialMainWindow::on_twist);
 }
 
 SineDialMainWindow::~SineDialMainWindow() {
-    delete left;
-    delete right;
     _client.disconnectFromServer();
 }
 
@@ -57,8 +63,8 @@ void SineDialMainWindow::setupJackClient() {
     _client.connectToServer("sinedial");
     _audio_out[0] = _client.registerAudioOutPort("out_1");
     _audio_out[1] = _client.registerAudioOutPort("out_2");
-    _audio_ring_buffer[0] = new QtJack::AudioRingBuffer();
-    _audio_ring_buffer[1] = new QtJack::AudioRingBuffer();
+    _audio_ring_buffer[0] = QtJack::AudioRingBuffer(48000 * 10);
+    _audio_ring_buffer[1] = QtJack::AudioRingBuffer(48000 * 10);
     _client.setMainProcessor(this);
     _client.activate();
 }
@@ -68,9 +74,47 @@ void SineDialMainWindow::on_twist() {
     _value_mod_freq = sinedial_ui->dial_mod_freq->value();
 }
 
+void SineDialMainWindow::timerEvent(QTimerEvent*) {
+    size_t ring_buffer_right_size =
+        _audio_ring_buffer[0].numberOfElementsAvailableForRead();
+    size_t ring_buffer_left_size =
+        _audio_ring_buffer[1].numberOfElementsAvailableForRead();
+
+   qDebug() << "Samples remaining in the ring buffer: "
+             << ring_buffer_right_size
+             << " / "
+             << ring_buffer_left_size;
+    //create 12000sine wave samples for 0.25seconds
+    if (ring_buffer_left_size < 24000) {
+        //todo function for left and right
+        sine_wave(left,
+            _value_base_freq,
+            _value_mod_freq,
+            generated_samples_per_tick,
+            samples_offset_left,
+            sample_rate);
+        _audio_ring_buffer[0].write(left, generated_samples_per_tick);
+        samples_offset_left += generated_samples_per_tick;
+    }
+    if (ring_buffer_left_size < 24000) {
+      // TODo sinewave generation for right side
+        sine_wave(right,
+            _value_base_freq,
+            _value_mod_freq,
+            generated_samples_per_tick,
+            samples_offset_right,
+            sample_rate);
+        _audio_ring_buffer[1].write(right, generated_samples_per_tick);
+        samples_offset_right += generated_samples_per_tick;
+    }
+}
+
+
 void SineDialMainWindow::process(int samples) {
     QtJack::AudioBuffer port_buffer_0 = _audio_out[0].buffer(samples);
+    port_buffer_0.pop(_audio_ring_buffer[0]);
     QtJack::AudioBuffer port_buffer_1 = _audio_out[1].buffer(samples);
+    port_buffer_1.pop(_audio_ring_buffer[1]);
 }
 
 void SineDialMainWindow::sine_wave_generate_function() {
@@ -78,12 +122,12 @@ void SineDialMainWindow::sine_wave_generate_function() {
             _value_base_freq,
             _value_mod_freq,
             generated_samples_per_tick,
-            samples_offset,
+            samples_offset_left,
             sample_rate);
   sine_wave(right,
             _value_base_freq,
             _value_mod_freq,
             generated_samples_per_tick,
-            samples_offset,
+            samples_offset_right,
             sample_rate);
 }
